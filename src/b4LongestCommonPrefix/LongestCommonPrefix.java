340183978a13e923e318871f135e85408e94b4db
ikonon <315793076@qq.com>
2018-06-17 10:55:59 +0800

    D7 2018/6/17 完善多种解法

--- src/b4LongestCommonPrefix/LongestCommonPrefix.java
+++ src/b4LongestCommonPrefix/LongestCommonPrefix.java
@@ -2,8 +2,9 @@ package b4LongestCommonPrefix;
 
 public class LongestCommonPrefix {
     /**
-     * @return prexi
+     * O(S) S是字符串中所有字符的集合 O(n)
      * @author ikonon
+     * @return prefix
      */
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
@@ -17,8 +18,24 @@ public class LongestCommonPrefix {
         return prefix;
     }
 
+    /**
+     * O(n^2)
+     * @author classic
+     */
+    public String longestCommonPrefix1(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
     public static void main(String[] args) {
-        String str = new LongestCommonPrefix().longestCommonPrefix(new String[]{"leet", "leetcode", "lee"});
+        String str = new LongestCommonPrefix().longestCommonPrefix1(new String[]{"leet", "leetcode", "lee"});
         System.out.println(str);
 
     }
